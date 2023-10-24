f62ad2a96da33086fd7a0210b0f5305b821c1a9b
Tenzo <61557738+mahdiennour@users.noreply.github.com>
2023-10-24 02:19:07 +0200

    Update UserServiceImplMock.java

--- src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
@@ -11,12 +11,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @TestMethodOrder(OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplMock {
-  User user = new User("f1", "l1", new Date(), Role.ADMINISTRATEUR);
-List<User> listUsers = new ArrayList<User>() {
-{
-add(new User("f2", "l2", new Date(), Role.ADMINISTRATEUR));
-add(new User("f3", "l3", new Date(), Role.ADMINISTRATEUR));
-}
-};
 }
 
