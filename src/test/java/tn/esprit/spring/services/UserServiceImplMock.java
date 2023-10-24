b7fd3b0965349b54c5724d20efcff29dcf8bfe63
Tenzo <61557738+mahdiennour@users.noreply.github.com>
2023-10-24 02:58:48 +0200

    Update UserServiceImplMock.java

--- src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
@@ -6,10 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Test
-public void testRetrieveUser() {
-Mockito.when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
-User user1 = userService.retrieveUser("2");
-Assertions.assertNotNull(user1);
-}
 
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplMock { }
